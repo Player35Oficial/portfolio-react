2304d0a35d9d1d9d30dfeeb6dc2a1a03e27fab43
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-03-26 23:04:38 -0300

    Repositórios Dinâmicos

--- src/components/MyProjects/Index.jsx
+++ src/components/MyProjects/Index.jsx
@@ -1,46 +1,24 @@
 import React from "react";
-import folder from "../../assets/folder.svg";
-import star from "../../assets/star.svg";
-import branch from "../../assets/git-branch.svg";
+import Project from "./Project";
+
 import "./Index.css";
 
 export default function MyProjects() {
-  console.log("carregou");
   return (
     <>
       <div className="container-title">
         <h2>
-          Meus Projetos <a href="/">Veja Todos</a>
+          Meus Projetos{" "}
+          <a
+            href="https://github.com/Player35Oficial?tab=repositories"
+            target="_blank"
+          >
+            Veja Todos
+          </a>
         </h2>
       </div>
-
-      <div className="project-container">
-        <div className="project-title">
-          <img src={folder} alt="ico" />
-          <h3>my-onix</h3>
-        </div>
-
-        <div className="project-description">
-          <p>
-            Consulte os códigos de erro que aparecem no painel do veículo Onix.
-          </p>
-        </div>
-
-        <div className="project-stats">
-          <div>
-            <span>
-              <img src={star} alt="star" />
-              100
-            </span>
-            <span>
-              <img src={branch} alt="branch" />
-              100
-            </span>
-          </div>
-          <div className="language javascript">
-            <span>Javascript</span>
-          </div>
-        </div>
+      <div className="container-project">
+        <Project />
       </div>
 
       <div className="container-title">
---
+++ src/components/MyProjects/Project/Index.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import folder from "../../../assets/folder.svg";
+import star from "../../../assets/star.svg";
+import branch from "../../../assets/git-branch.svg";
+
+export default function Project() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/player35Oficial/repos")
+      .then((resp) => resp.json())
+      .then((data) => {
+        data = data.filter((current, indice) => current && indice < 2);
+        setRepos(data);
+      });
+  }, []);
+
+  return (
+    <>
+      {repos.map((repo) => (
+        <div className="project-container">
+          <div className="project-title">
+            <img src={folder} alt="ico" />
+            <h3>{repo.name}</h3>
+          </div>
+
+          <div className="project-description">
+            {repo.description ? (
+              <p>{repo.description}</p>
+            ) : (
+              <p>{repo.full_name}</p>
+            )}
+          </div>
+
+          <div className="project-stats">
+            <div>
+              <span>
+                <img src={star} alt="star" />
+                {repo.stargazers_count}
+              </span>
+              <span>
+                <img src={branch} alt="branch" />
+                {repo.forks}
+              </span>
+            </div>
+            <div className={`language ${repo.language}`}>
+              <span>{repo.language || "Ausente"}</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
